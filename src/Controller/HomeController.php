b5a930e7e373be308bbe60d5f410f22223c9b118
Benj <benbourgeois@hotmail.fr>
2022-09-06 10:58:54 +0200

    liste+show chambre

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,9 +3,10 @@
 namespace App\Controller;
 
 use App\Repository\SliderRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ChambreRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
@@ -15,6 +16,24 @@ class HomeController extends AbstractController
         $sliders = $repo->findBy([], ['ordre' => 'ASC']);
         return $this->render('home/index.html.twig', [
             'sliders' => $sliders,
+        ]);
+    }
+
+    #[Route('/chambres', name: 'app_chambres')]
+    public function listeChambre(ChambreRepository $repo): Response
+    {
+        $chambres = $repo->findAll();
+        return $this->render('home/chambres.html.twig', [
+            'tabChambres' => $chambres,
+        ]);
+    }
+
+    #[Route('/chambre/{id}', name: 'app_chambre')]
+    public function showChambre(ChambreRepository $repo, $id): Response
+    {
+        $chambre = $repo->find($id);
+        return $this->render('home/showChambre.html.twig', [
+            'chambre' => $chambre,
         ]);
     }
 }
