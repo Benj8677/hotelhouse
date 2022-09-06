74b48361a2291c5ffe9110a0df09c5cbddd988ee
Benj <benbourgeois@hotmail.fr>
2022-09-06 12:24:09 +0200

    cart v1 + favicon

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\CartService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class CartController extends AbstractController
 {
     #[Route('/cart', name: 'app_cart')]
-    public function index(): Response
+    public function index(CartService $cs): Response
     {
+        $cartWithData = $cs->getCartWithData();
+        $cs->setNbProduct();
+        $cs->setTotalCart();
+
         return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
+            'items' => $cartWithData,
         ]);
     }
 
@@ -23,5 +28,45 @@ class CartController extends AbstractController
         return $this->render('cart/compte.html.twig', [
             'membre' => $user,
         ]);
+    }
+
+    #[Route('/cartend', name: 'app_cart_end')]
+    public function end(CartService $cs): Response
+    {
+        $cartWithData = $cs->getCartWithData();
+        $cs->setNbProduct();
+        $cs->setTotalCart();
+
+        return $this->render('cart/end.html.twig', [
+            'items' => $cartWithData,
+        ]);
+    }
+
+    #[Route('/cart/add/{id}', name:'cart_add')]
+    public function add($id, CartService $cs)
+    {
+        $cs->add($id);
+        return $this->redirectToRoute('app_cart');
+    }
+
+    #[Route('/cart/minus/{id}', name:'cart_min')]
+    public function decrement($id, CartService $cs)
+    {
+        $cs->minus($id);
+        return $this->redirectToRoute('app_cart');
+    }
+
+    #[Route('/cart/remove/{id}', name:'cart_remove')]
+    public function remove($id, CartService $cs)
+    {
+        $cs->remove($id);
+        return $this->redirectToRoute('app_cart');
+    }
+
+    #[Route('/cart/removeAll', name:'cart_destroy')]
+    public function removeAll(CartService $cs)
+    {
+        $cs->removeAll();
+        return $this->redirectToRoute('app_cart');
     }
 }
